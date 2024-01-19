82cca796d07fb5503517c23444da9817973749bc
f-developer0507 <stormpubg0507@gmail.com>
2024-01-19 09:34:55 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// dark mode
+const mode = document.querySelector(".mode");
+const body = document.querySelector("body");
+
+mode.addEventListener("click", function(){
+  body.classList.toggle("dark-mode")
+});
+
+// burger
+const burgerBtn = document.querySelector(".burger-btn");
+const headerList = document.querySelector(".header__list");
+
+burgerBtn.addEventListener("click",function(){
+  burgerBtn.classList.toggle("active")
+  headerList.classList.toggle("header__list--mobile")
+})
