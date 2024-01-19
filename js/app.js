cf7d0f7f7354630b1b05abc40c2161c45109b611
f-developer0507 <stormpubg0507@gmail.com>
2024-01-19 10:36:09 +0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
+// loader
+const loader = document.querySelector(".loader-wrapper");
+
+window.onload = function () {
+  loader.classList.add("loader-hidden");
+  setInterval(function () {
+    loader.classList.add("loader-hidden");
+  },990);
+};
+
 // dark mode
 const mode = document.querySelector(".mode");
 const body = document.querySelector("body");
 
-mode.addEventListener("click", function(){
-  body.classList.toggle("dark-mode")
+mode.addEventListener("click", function () {
+  body.classList.toggle("dark-mode");
 });
 
 // burger
 const burgerBtn = document.querySelector(".burger-btn");
 const headerList = document.querySelector(".header__list");
 
-burgerBtn.addEventListener("click",function(){
-  burgerBtn.classList.toggle("active")
-  headerList.classList.toggle("header__list--mobile")
-})
+burgerBtn.addEventListener("click", function () {
+  burgerBtn.classList.toggle("active");
+  headerList.classList.toggle("header__list--mobile");
+});
